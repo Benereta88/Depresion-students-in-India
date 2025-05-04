74dd7a0ec41f45b27749dd71e54524e01d9c6d73
Benereta Hox <beneretahoxha8@gmail.com>
2025-05-04 21:43:35 +0200

    skapat en csv

--- js/_menu.js
+++ js/_menu.js
@@ -1,8 +1,7 @@
-createMenu('Statistics Template JS', [
+createMenu('Psykisk ohälsa bland studerande', [
 
   { name: 'Familjhistorisk och suicidaltankar påverkan ', script: 'familjhistorisk-suicidaltankar.js' },
   { name: 'Sömnens längd påverkan ', script: 'trends-av-somnens.js' },
   { name: 'Jämför akademisk & jobbrelaterad press ', script: 'compare-akademisk-jobbrelaterad-press.js' },
   { name: 'Skriftligt analys ', script: 'Analys-skriflig.js' },
-  { name: 'Tabel', script: 'tabell.js' }
 ]);
